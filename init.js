372b7964babbef5098bd2c3887c9e4dde007e691
JoyDaheeCha <ekgmlekgml20@gmail.com>
2019-10-11 18:25:01 +0900

    Add Video model

--- init.js
+++ init.js
@@ -2,8 +2,11 @@ import "./db";
 import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
+import "./models/Video"
 
 const PORT = process.env.PORT || 4000;
-const handleListening = () => console.log(`Listening on: http://localhosst:${PORT}`);
+
+const handleListening = () =>
+    console.log(`Listening on: http://localhosst:${PORT}`);
 
 app.listen(PORT, handleListening);
