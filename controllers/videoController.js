1849c9fcfd84691974d3c33515f1011f22f54cd6
JoyDaheeCha <daheecha0612@gmail.com>
2019-10-16 15:49:31 +0900

    Add creator name
    Added creator name to
    - UI: Video Detail page
    - Model : Video, Comments
    
    Added comments and videos to user model to connect the three models.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -61,8 +61,11 @@ export const postUpload = async (req, res) => {
     const newVideo = await Video.create({
         fileUrl: path,
         title,
-        description
+        description,
+        creator: req.user.id
     });
+    req.user.videos.push(newVideo.id);
+    req.user.save();
     res.redirect(routes.videoDetail(newVideo.id));
 };
 
@@ -73,7 +76,8 @@ export const videoDetails = async (req, res) => {
         }
     } = req;
     try {
-        const video = await Video.findById(id);
+        const video = await Video.findById(id).populate('creator');
+        console.log(video);
         res.render("videoDetail", {
             pageTitle: video.title,
             video
