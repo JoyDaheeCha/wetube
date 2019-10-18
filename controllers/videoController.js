afb557032364ade588f2c8f32174232da081afa1
JoyDaheeCha <daheecha0612@gmail.com>
2019-10-18 19:15:20 +0900

    Change file path from local to online server
    Change video file and avatar picture's location from local to AWS S3 server.

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -39,10 +39,11 @@ export const getUpload = (req, res) => res.render("upload", { pageTitle: "Upload
 export const postUpload = async (req, res) => {
   const {
     body: { title, description },
-    file: { path }
+    file: { location }
   } = req;
+  console.log(req.file);
   const newVideo = await Video.create({
-    fileUrl: path,
+    fileUrl: location,
     title,
     description,
     creator: req.user.id
--- middlewares.js
+++ middlewares.js
@@ -1,31 +1,53 @@
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 import routes from "./routes";
 
-export const multerVideo = multer({dest: "uploads/videos/"});
-export const multerAvatar = multer({dest: "uploads/avatars/"});
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY,
+  secretAccessKey: process.env.AWS_SECRET,
+  region: "ap-northeast-2"
+});
 
+export const multerVideo = multer({
+  storage: multerS3({
+    s3,
+    acl: "public-read",
+    bucket: "wetube-joy/video"
+  })
+});
+
+//export const multerAvatar = multer({ dest: "uploads/avatars/" });
+
+export const multerAvatar = multer({
+    storage: multerS3({
+        s3,
+        acl: "public-read",
+        bucket: "wetube-joy/avatar"
+    })
+})
 export const localsMiddleware = (req, res, next) => {
-    res.locals.siteName = "WeTube";
-    res.locals.routes = routes;
-    res.locals.loggedUser = req.user || null;
-    next();
-}
-
-export const onlyPublic = (req, res, next) => {
-    if(req.user){
-        res.redirect(routes.home);
-    }else{
-        next();
-    }
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  res.locals.loggedUser = req.user || null;
+  next();
 };
 
-export const onlyPrivate = (req, res, next) =>{
-    if(req.user){
-        next();
-    }else{
-        res.redirect(routes.home);
-    }
-}
+export const onlyPublic = (req, res, next) => {
+  if (req.user) {
+    res.redirect(routes.home);
+  } else {
+    next();
+  }
+};
 
-export const uploadVideo = multerVideo.single('fileUrl');
-export const uploadAvatar = multerAvatar.single('avatar');
+export const onlyPrivate = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect(routes.home);
+  }
+};
+
+export const uploadVideo = multerVideo.single("fileUrl");
+export const uploadAvatar = multerAvatar.single("avatar");
